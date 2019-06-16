01546a003e22afded3e8ec41eb2b70099475c96c
kluxa <k.luxa@student.unsw.edu.au>
2019-06-16 19:21:18 +1000

    updated build.sh to compile solutions

--- practice/singlyLinkedLists/build.sh
+++ practice/singlyLinkedLists/build.sh
@@ -20,6 +20,12 @@ then
 	directory="solutions/"
 fi
 
+if [ ! -e "exercises/$fname.c" ]
+then
+	echo "$0: no activity named '$fname'"
+	exit 1
+fi
+
 upper="${fname^}"
 gcc -Wall -Werror -std=gnu99 -g -o testList list.c "testDrivers/test$upper.c" "$directory/$fname.c"
 
